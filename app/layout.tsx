7e5a4d9421d68d1e828acb6ad1da7305174e80fc
Omar Cardy <cardyomar@gmail.com>
2026-06-10 12:45:38 +0400

    Update favicon to use logo icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,21 +25,8 @@ export const metadata: Metadata = {
     google: 'VKwaZdAUrzpO-L_ozYeJpWPEEMH75Uzljc5k4RCoS98',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/icon.png',
+    apple: '/icon.png',
   },
 }
 
